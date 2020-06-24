417f824422074491e58cc183c92ff1a5bdeaae86
Wee <983065876@qq.com>
2020-06-24 19:04:43 +0800

    摄像机聚焦多个Actor

---
+++ Source/my2Dtry/Private/CameraGuideAnchorComp.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CameraGuideAnchorComp.h"
+
+UCameraGuideAnchorComp::UCameraGuideAnchorComp(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	focusWeight = 1.0f;
+}
---
+++ Source/my2Dtry/Public/CameraGuideAnchorComp.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "CameraGuideAnchorComp.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, ClassGroup = "CameraGuide", hidecategories = (Object, LOD, Lighting, TextureStreaming), editinlinenew, meta = (DisplayName = "Camera Guide Anchor", BlueprintSpawnableComponent))
+class MY2DTRY_API UCameraGuideAnchorComp : public UBoxComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UCameraGuideAnchorComp(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float focusWeight;
+};
