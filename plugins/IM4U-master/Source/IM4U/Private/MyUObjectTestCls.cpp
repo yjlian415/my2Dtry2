ec09d6c584e338aefe10bb006f0fea6817408240
yjlian415 <983065876@qq.com>
2021-03-04 22:27:07 +0800

    删除IM4U

--- plugins/IM4U-master/Source/IM4U/Private/MyUObjectTestCls.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2015 BlackMa9. All Rights Reserved.
-
-#include "MyUObjectTestCls.h"
-#include "IM4UPrivatePCH.h"
-
-UMyUObjectTestCls::UMyUObjectTestCls(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
-{
-}
